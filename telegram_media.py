b8e41f11ce62e162d2247ec68e7388a9f6adadc0
Tim Smith <tim.smith@vaultcloud.com.au>
2020-05-22 18:20:42 +1000

    use commandline args, add list-channels, tweak filename

--- telegram_media.py
+++ telegram_media.py
@@ -1,29 +1,60 @@
+import argparse
 import os
+
+from telethon import types
 from telethon.sync import TelegramClient
-# Refer to the documentation for other filter types.
-# https://tl.telethon.dev/types/messages_filter.html
-from telethon.tl.types import InputMessagesFilterPhotoVideo
 
-api_id = os.environ['api_id']
-api_hash = os.environ['api_hash']
-channel = int(os.environ['channel'])
-limit = int(os.environ.get('limit', 10))
 
-with TelegramClient('session_name', api_id, api_hash) as client:
+def main():
+    args = get_args()
 
-    for message in client.iter_messages(channel,
-                                        limit=limit,
-                                        filter=InputMessagesFilterPhotoVideo):
+    # use env vars if they're provided, otherwise use command-line args
+    api_id = os.environ.get('api-id', args.api_id)
+    api_hash = os.environ.get('api-hash', args.api_hash)
+    channel = int(os.environ.get('channel', args.channel))
+    limit = int(os.environ.get('limit', args.limit))
 
-        filename = (f'{message.date.year}'
-                    f'{message.date.month}-'
-                    f'{message.text or "unknown"}-'
-                    f'{message.file.id[-8:]}'
-                    f'{message.file.ext}'
-                    )
+    with TelegramClient('session_name', api_id, api_hash) as client:
 
-        if os.path.isfile(filename):
-            print(f'{filename} already exists. Skipping.')
+        if args.list_channels:
+            list_channel_ids(client)
         else:
-            print(f'{filename} downloading...')
-            path = message.download_media(file=filename)
+            # Refer to the documentation for other filter types.
+            # https://tl.telethon.dev/types/messages_filter.html
+            message_filter = types.InputMessagesFilterPhotoVideo
+
+            for message in client.iter_messages(channel, limit=limit, filter=message_filter):
+                # You lose the original filename when you share a file in a Telegram chat.
+                # So we construct a new, and hopefully unique, filename.
+                # TODO use inbuilt path module
+                filename = '_'.join([
+                    message.date.strftime("%Y%m%d"),
+                    str(message.chat_id),
+                    message.file.id,
+                    message.file.ext
+                ])
+
+                if os.path.isfile(filename):
+                    print(f'File already exists: {filename}')
+                else:
+                    print(f'Downloading: {filename}')
+                    path = message.download_media(file=filename)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Download media from a Telegram channel (or private chat).')
+    parser.add_argument("--api-id", help="API ID")
+    parser.add_argument("--api-hash", help="API Hash")
+    parser.add_argument("--list-channels", help="List all channel IDs", action="store_true")
+    parser.add_argument("--channel", help="Channel ID")
+    parser.add_argument("--limit", help="Only download the last n items", default=0)
+    return parser.parse_args()
+
+
+def list_channel_ids(client):
+    for dialog in client.iter_dialogs():
+        print(f'{dialog.name}: {dialog.id}')
+
+
+if __name__ == "__main__":
+    main()
